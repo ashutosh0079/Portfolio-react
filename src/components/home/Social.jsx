75b1f812ea102acacc2f40e494f249282d0372c1
Ashutosh Gupta <ashu07988@gmail.com>
2024-06-01 23:53:11 +0530

    Update Social.jsx

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Social = () => {
   return (
     <div className='home__social'>
-        <a href="" className="home__socail-icon" target='_blank'>
+        <a href="https://www.instagram.com/47._.ashu?igsh=MWU2OHhhaWZjeHQwaA==" className="home__socail-icon" target='_blank'>
             <i class="uil uil-instagram"></i>
         </a>
         <a href="" className="home__socail-icon" target='_blank'>
         <i class="uil uil-dribbble"></i>
         </a>
-        <a href="" className="home__socail-icon" target='_blank'>
+        <a href="https://github.com/ashutosh0079" className="home__socail-icon" target='_blank'>
         <i class="uil uil-github-alt"></i>
         </a>
     </div>
