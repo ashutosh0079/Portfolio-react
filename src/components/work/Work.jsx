94089fc36c53c08c84ff2f1ff3555d4c6e16ec67
Ashutosh Gupta <ashu07988@gmail.com>
2024-06-01 23:43:17 +0530

    Update Work.jsx

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -4,7 +4,7 @@ import Works from './Works';
 const Work = () => {
   return (
     <section className="work section" id='portfolio'>
-        <h2 className="section__title">Porftfolio</h2>
+        <h2 className="section__title">Portfolio</h2>
         <span className="section__subtitle">Most recent Works</span>
 
         <Works/>
