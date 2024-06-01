3fb1cb6687fbae327e2a7a18ee8849a1c3f2ea73
Ashutosh Gupta <ashu07988@gmail.com>
2024-06-01 15:52:48 +0530

    first commit

---
+++ src/components/qualification/Qualification.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from 'react'
+import "./qualification.css"
+const Qualification = () => {
+    const [toggleState, setToggleState] = useState(1);
+    const toggleTab = (index) =>{
+        setToggleState(index);
+    } 
+  return (
+    <section className="qualification sections">
+        <h2 className="section__title">Qualification</h2>
+        <span className="section__subtitle">My Personal journey</span>
+        <div className="qualification__conatiner container">
+            <div className="qualification__tabs">
+                <div className={toggleState ===1 ? "qualification__button qualification__active button--flex": "qualification__button button--flex"}>
+                    <i className="uil uil-graduation-cap qualification__icon">
+                    </i>{" "}Education
+                </div>
+
+                <div className="qualification__button button--flex">
+                    <i className="uil uil-graduation-cap qualification__icon">
+                    </i>{" "}Experience
+                </div>
+            </div>
+
+            <div className="qualification__sections">
+                <div className={toggleState===1 ?"qualification__content qualification__content-active": "qualification__content"}>
+                    <div className="qualification__data">
+
+                        <div>
+                            <h3 className="qualification__title">Bachlors in Computer Science</h3>
+                            <span className="qualification__subtitle">Vellore Instiute of technology, VIT </span>
+                            <div className="qualification__calender">
+                                <i className="uil uil-calender-alt"></i>2020-2024
+                            </div>
+                        </div>
+                        <div>
+                            <span className='qualification__rounder'></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                    </div>
+
+                    <div className="qualification__data">
+                        <div></div>
+
+                        <div>
+                            <span className='qualification__rounder'></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                        <div>
+                            <h3 className="qualification__title">PHP Developer Internship</h3>
+                            <span className="qualification__subtitle">MAWAI INFOTECH LTD.</span>
+                            <div className="qualification__calender">
+                                <i className="uil uil-calender-alt"></i>June-July,2023
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                    <div className="qualification__data">
+
+                        <div>
+                            <h3 className="qualification__title">Secondary High School</h3>
+                            <span className="qualification__subtitle">Percentage: 90</span>
+                            <div className="qualification__calender">
+                                <i className="uil uil-calender-alt"></i>2019-2020
+                            </div>
+                        </div>
+                        <div>
+                            <span className='qualification__rounder'></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                    </div>
+
+                    <div className="qualification__data">
+                        <div></div>
+
+                        <div>
+                            <span className='qualification__rounder'></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                        <div>
+                            <h3 className="qualification__title">JPMorgan Chase & Co. Virtual Internship</h3>
+                            <span className="qualification__subtitle">Forage</span>
+                            <div className="qualification__calender">
+                                <i className="uil uil-calender-alt"></i>June, 2023
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Qualification
---
+++ src/components/work/Data.jsx
@@ -0,0 +1,52 @@
+import Work1 from "../../assets/work1.jpg";
+import Work2 from "../../assets/work2.jpg";
+import Work3 from "../../assets/work3.jpg";
+import Work4 from "../../assets/work4.jpg";
+import Work5 from "../../assets/work5.jpg";
+
+export const projectsData = [
+    {
+      id: 1,
+      image: Work1,
+      title: "Booking MERN",
+      category: "web",
+    },
+    {
+      id: 2,
+      image: Work2,
+      title: "Property Pass",
+      category: "app",
+    },
+    {
+      id: 3,
+      image: Work3,
+      title: "Sales dashboard(POWERBI)",
+      category: "design",
+    },
+    {
+      id: 4,
+      image: Work4,
+      title: "Quiz webite",
+      category: "app",
+    },
+    {
+      id: 5,
+      image: Work5,
+      title: "Netflix clone",
+      category: "web",
+    },
+  ];
+  export const projectsNav = [
+    {
+        name:'all',
+    },
+    {
+        name:'web',
+    },
+    {
+        name:'app',
+    },
+    {
+        name:'design',
+    },
+  ];
