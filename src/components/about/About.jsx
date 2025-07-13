0c0e8986116fb4789203acd9a8f29119a868e1b0
Ashutosh Gupta <ashu07988@gmail.com>
2025-07-13 23:46:07 +0530

    Update profile

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./about.css"
-import AboutImg from "../../assets/new_profile.jpg"
+import AboutImg from "../../assets/new_profile.jpeg"
 import cv from "../../assets/Resume__final.pdf"
 import Info from './Info'
 
