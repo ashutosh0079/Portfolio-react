d57fef75a601f54531584b698c679b8ba4672242
Ashutosh Gupta <ashu07988@gmail.com>
2026-06-23 00:11:27 +0530

    Update Data.jsx

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -10,7 +10,7 @@ const Data = () => {
                   viewBox="0 0 48 48"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
-                  class="home__hand"
+                  className="home__hand"
                 >
                   <path
                     d="M25.4995 32.0305L31.3495 33.1555L36.1495 8.48051C36.4495 6.83051 35.3995 5.18051 33.8245 4.88051C32.1745 4.58051 30.5995 5.70551 30.2995 7.35551L25.4995 32.0305Z"
@@ -54,15 +54,18 @@ const Data = () => {
                   ></path>
                 </svg>
         </h1>
-        <h3 className="home__subtitle">Web Developer</h3>
+        <h3 className="home__subtitle">Data Analyst | AI Enthusiast</h3>
         <p className="home__description">
-        I’m passionate web developer with a knack for creating dynamic, user-friendly websites and applications.
+        I'm a Data Analyst with experience in SQL, Excel, Python, and Power BI.
+        Enjoy transforming complex data into meaningful insights, building
+        interactive dashboards, and automating reporting processes to support
+        data-driven decision-making.
         </p>
         <div style={{ marginBottom: '20px' }}></div>
         <a href="#contact" className="button button--flex">
             Say Hello
             <svg
-                  class="button__icon"
+                  className="button__icon"
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
                   height="24"
