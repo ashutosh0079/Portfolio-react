0d7e43e1b74dce5cf9e8b8eb0eb01307e763ef63
Ashutosh Gupta <ashu07988@gmail.com>
2026-06-22 23:12:53 +0530

    Update Social.jsx

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Social = () => {
   return (
-    <div className='home__social'>
-        <a href="https://www.instagram.com/47._.ashu?igsh=MWU2OHhhaWZjeHQwaA==" className="home__socail-icon" target='_blank'>
-            <i class="uil uil-instagram"></i>
-        </a>
-        <a href="" className="home__socail-icon" target='_blank'>
-        <i class="uil uil-dribbble"></i>
-        </a>
-        <a href="https://github.com/ashutosh0079" className="home__socail-icon" target='_blank'>
-        <i class="uil uil-github-alt"></i>
-        </a>
-    </div>
-  )
-}
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/47._.ashu"
+        className="home__social-icon"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="uil uil-instagram"></i>
+      </a>
 
-export default Social
+      <a
+        href="https://github.com/ashutosh0079"
+        className="home__social-icon"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
