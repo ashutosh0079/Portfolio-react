4883a6f88bc2136b486095d55bb57feb49b64f52
Ashutosh Gupta <ashu07988@gmail.com>
2026-06-23 00:35:07 +0530

    Update Qualification.jsx

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -2,50 +2,40 @@ import React from "react";
 import "./qualification.css";
 
 const Qualification = () => {
-  const toggleState = 1;
-
   return (
-    <section className="qualification sections">
-      <h2 className="section__title">Qualification</h2>
-      <span className="section__subtitle">My Personal Journey</span>
+    <section className="qualification section" id="qualification">
+      <h2 className="section__title">Education & Experience</h2>
+      <span className="section__subtitle">
+        My Academic & Professional Journey
+      </span>
 
-      <div className="qualification__conatiner container">
+      <div className="qualification__container container">
         <div className="qualification__tabs">
-          <div
-            className={
-              toggleState === 1
-                ? "qualification__button qualification__active button--flex"
-                : "qualification__button button--flex"
-            }
-          >
-            <i className="uil uil-graduation-cap qualification__icon"></i>{" "}
+          <div className="qualification__button qualification__active button--flex">
+            <i className="uil uil-graduation-cap qualification__icon"></i>
             Education
           </div>
 
-          <div className="qualification__button button--flex">
-            <i className="uil uil-graduation-cap qualification__icon"></i>{" "}
+          <div className="qualification__button qualification__active button--flex">
+            <i className="uil uil-briefcase-alt qualification__icon"></i>
             Experience
           </div>
         </div>
 
         <div className="qualification__sections">
-          <div
-            className={
-              toggleState === 1
-                ? "qualification__content qualification__content-active"
-                : "qualification__content"
-            }
-          >
+          <div className="qualification__content qualification__content-active">
+
+            {/* Education */}
             <div className="qualification__data">
               <div>
                 <h3 className="qualification__title">
-                  Bachelor's in Computer Science
+                  Bachelor of Technology in Computer Science
                 </h3>
                 <span className="qualification__subtitle">
                   Vellore Institute of Technology (VIT)
                 </span>
-                <div className="qualification__calender">
-                  <i className="uil uil-calender-alt"></i> 2020 - 2024
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> 2020 – 2024
                 </div>
               </div>
 
@@ -55,6 +45,7 @@ const Qualification = () => {
               </div>
             </div>
 
+            {/* Experience */}
             <div className="qualification__data">
               <div></div>
 
@@ -65,27 +56,28 @@ const Qualification = () => {
 
               <div>
                 <h3 className="qualification__title">
-                  PHP Developer Internship
+                  Data Analyst
                 </h3>
                 <span className="qualification__subtitle">
-                  MAWAI INFOTECH LTD.
+                  Paytm
                 </span>
-                <div className="qualification__calender">
-                  <i className="uil uil-calender-alt"></i> June – July 2023
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> 2024 – Present
                 </div>
               </div>
             </div>
 
+            {/* School */}
             <div className="qualification__data">
               <div>
                 <h3 className="qualification__title">
-                  Secondary High School
+                  Senior Secondary School
                 </h3>
                 <span className="qualification__subtitle">
                   Percentage: 90%
                 </span>
-                <div className="qualification__calender">
-                  <i className="uil uil-calender-alt"></i> 2019 - 2020
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> 2019 – 2020
                 </div>
               </div>
 
@@ -95,24 +87,27 @@ const Qualification = () => {
               </div>
             </div>
 
+            {/* AI Internship */}
             <div className="qualification__data">
               <div></div>
 
               <div>
                 <span className="qualification__rounder"></span>
-                <span className="qualification__line"></span>
               </div>
 
               <div>
                 <h3 className="qualification__title">
-                  JPMorgan Chase &amp; Co. Virtual Internship
+                  Data Science Internship
                 </h3>
-                <span className="qualification__subtitle">Forage</span>
-                <div className="qualification__calender">
-                  <i className="uil uil-calender-alt"></i> June 2023
+                <span className="qualification__subtitle">
+                  Cepta Infotech
+                </span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> 2024
                 </div>
               </div>
             </div>
+
           </div>
         </div>
       </div>
