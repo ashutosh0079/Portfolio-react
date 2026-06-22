49e8ddf340dea468b873dc6ca96231227f977356
Ashutosh Gupta <ashu07988@gmail.com>
2026-06-23 00:23:21 +0530

    Update Frontend.jsx

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -3,62 +3,65 @@ import React from 'react'
 const Frontend = () => {
   return (
     <div className='skills__content'>
-        <h3 className="skills__title">Frontend Developer</h3>
-        <div className="skills__box">
-            <div className="skills__group">
+      <h3 className="skills__title">📊 Data Analytics</h3>
 
-                <div className="skills__data">
-                    <i class='bx bx-badge-check'></i>
-                    <div>
-                        <h3 className="skills__name">HTML</h3>
-                        <span className="skills__level">Intermediate</span>
-                    </div>
-                </div>
+      <div className="skills__box">
+        <div className="skills__group">
 
-                <div className="skills__data">
-                    <i class='bx bx-badge-check'></i>
-                    <div>
-                        <h3 className="skills__name">CSS</h3>
-                        <span className="skills__level">Advanced</span>
-                    </div>
-                </div>
-
-                <div className="skills__data">
-                    <i class='bx bx-badge-check'></i>
-                    <div>
-                        <h3 className="skills__name">Javascript</h3>
-                        <span className="skills__level">Intermediate</span>
-                    </div>
-                </div>
+          <div className="skills__data">
+            <i className='bx bx-badge-check'></i>
+            <div>
+              <h3 className="skills__name">SQL</h3>
+              <span className="skills__level">Advanced</span>
             </div>
+          </div>
 
-            <div className="skills__group">
-                <div className="skills__data">
-                    <i class='bx bx-badge-check'></i>
-                    <div>
-                        <h3 className="skills__name">Bootstrap</h3>
-                        <span className="skills__level">Intermediate</span>
-                    </div>
-                </div>
-
-                <div className="skills__data">
-                    <i class='bx bx-badge-check'></i>
-                    <div>
-                        <h3 className="skills__name">React</h3>
-                        <span className="skills__level">Intermediate</span>
-                    </div>
-                </div>
-
-                <div className="skills__data">
-                    <i class='bx bx-badge-check'></i>
-                    <div>
-                        <h3 className="skills__name">Git</h3>
-                        <span className="skills__level">Intermediate</span>
-                    </div>
-                </div>
-
+          <div className="skills__data">
+            <i className='bx bx-badge-check'></i>
+            <div>
+              <h3 className="skills__name">Excel</h3>
+              <span className="skills__level">Advanced</span>
             </div>
+          </div>
+
+          <div className="skills__data">
+            <i className='bx bx-badge-check'></i>
+            <div>
+              <h3 className="skills__name">Power BI</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+
         </div>
+
+        <div className="skills__group">
+
+          <div className="skills__data">
+            <i className='bx bx-badge-check'></i>
+            <div>
+              <h3 className="skills__name">Python</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className='bx bx-badge-check'></i>
+            <div>
+              <h3 className="skills__name">ETL Pipelining</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className='bx bx-badge-check'></i>
+            <div>
+              <h3 className="skills__name">Data Reconsillation</h3>
+              <span className="skills__level">Advanced</span>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
