0be344e9e3aa4f7402862f0aec1249e29860c648
Ashutosh Gupta <ashu07988@gmail.com>
2026-06-23 00:42:49 +0530

    Update Qualification.jsx

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -13,29 +13,24 @@ const Qualification = () => {
         <div className="qualification__tabs">
           <div className="qualification__button qualification__active button--flex">
             <i className="uil uil-graduation-cap qualification__icon"></i>
-            Education
-          </div>
-
-          <div className="qualification__button qualification__active button--flex">
-            <i className="uil uil-briefcase-alt qualification__icon"></i>
-            Experience
+            Timeline
           </div>
         </div>
 
         <div className="qualification__sections">
           <div className="qualification__content qualification__content-active">
 
-            {/* Education */}
+            {/* Senior Secondary */}
             <div className="qualification__data">
               <div>
                 <h3 className="qualification__title">
-                  Bachelor of Technology in Computer Science
+                  Senior Secondary School
                 </h3>
                 <span className="qualification__subtitle">
-                  Vellore Institute of Technology (VIT)
+                  Percentage: 90%
                 </span>
                 <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt"></i> 2020 – 2024
+                  <i className="uil uil-calendar-alt"></i> 2019 April – 2020 May
                 </div>
               </div>
 
@@ -45,13 +40,54 @@ const Qualification = () => {
               </div>
             </div>
 
-            {/* Experience */}
+            {/* B.Tech */}
             <div className="qualification__data">
               <div></div>
 
               <div>
                 <span className="qualification__rounder"></span>
                 <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">
+                  Bachelor of Technology in Computer Science
+                </h3>
+                <span className="qualification__subtitle">
+                  Vellore Institute of Technology (VIT)
+                </span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> 2020 Aug – 2024 July
+                </div>
+              </div>
+            </div>
+
+            {/* AI Internship */}
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">
+                  Artificial Intelligence Intern
+                </h3>
+                <span className="qualification__subtitle">
+                  Cepta Infotech
+                </span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> 2024 Aug - 2025 Jan
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            {/* Paytm */}
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
               </div>
 
               <div>
@@ -62,48 +98,7 @@ const Qualification = () => {
                   Paytm
                 </span>
                 <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt"></i> 2024 – Present
-                </div>
-              </div>
-            </div>
-
-            {/* School */}
-            <div className="qualification__data">
-              <div>
-                <h3 className="qualification__title">
-                  Senior Secondary School
-                </h3>
-                <span className="qualification__subtitle">
-                  Percentage: 90%
-                </span>
-                <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt"></i> 2019 – 2020
-                </div>
-              </div>
-
-              <div>
-                <span className="qualification__rounder"></span>
-                <span className="qualification__line"></span>
-              </div>
-            </div>
-
-            {/* AI Internship */}
-            <div className="qualification__data">
-              <div></div>
-
-              <div>
-                <span className="qualification__rounder"></span>
-              </div>
-
-              <div>
-                <h3 className="qualification__title">
-                  Data Science Internship
-                </h3>
-                <span className="qualification__subtitle">
-                  Cepta Infotech
-                </span>
-                <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt"></i> 2024
+                  <i className="uil uil-calendar-alt"></i> 2025 Jan – 2026 Mar
                 </div>
               </div>
             </div>
