4dae39f3049558833f86ac0adf68768e56e829c3
Ashutosh Gupta <ashu07988@gmail.com>
2026-06-22 23:06:57 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,42 +1,47 @@
-import React from 'react'
-import "./footer.css"
+import React from "react";
+import "./footer.css";
+
 const Footer = () => {
   return (
     <footer className="footer">
-        <div className="footer__container container">
-            <h1 className="footer__title">Ashutosh</h1>
-            <ul className="footer__list">
-                <li>
-                    <a href="#about" className="footer__link">About</a>
-                </li>
-                <li>
-                    <a href="#portfolio" className="footer__link">Projects</a>
-                </li>
-                <li>
-                    <a href="#skills" className="footer__link">Skills</a>
-                </li>
-            </ul>
+      <div className="footer__container container">
+        <h1 className="footer__title">Ashutosh</h1>
 
-            <div className="footer__social">
-            
-            <a href="" className="footer__social-link" target='_blank'>
-            <i class="bx bxl-facebook"></i>
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">
+              About
             </a>
+          </li>
+          <li>
+            <a href="#portfolio" className="footer__link">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="#skills" className="footer__link">
+              Skills
+            </a>
+          </li>
+        </ul>
 
-            <a href="https://www.instagram.com/47._.ashu?igsh=MWU2OHhhaWZjeHQwaA==" className="footer__social-link" target='_blank'>
-            <i class="bx bxl-instagram"></i>
-            </a>
-            
-            <a href="" className="footer__social-link" target='_blank'>
-            <i class="bx bxl-twitter"></i>
-            </a>
-            </div>
-            <span className='footer__copy'>
-                &#169; MrCoder. All rights reserved 
-            </span>
+        <div className="footer__social">
+          <a
+            href="https://www.instagram.com/47._.ashu"
+            className="footer__social-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="bx bxl-instagram"></i>
+          </a>
         </div>
-    </footer>
-  )
-}
 
-export default Footer
+        <span className="footer__copy">
+          &#169; MrCoder. All rights reserved
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
